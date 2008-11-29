f3be64f3077645d142d6578ae2370cff49b1930d
Brad Fitzpatrick <brad@danga.com>
2008-11-29 15:40:12 -0800

    remove unused crap, and add Updater class.

--- addressbooker.py
+++ addressbooker.py
@@ -16,11 +16,12 @@
 __author__ = 'brad@danga.com (Brad Fitzpatrick)'
 
 # Core Python
-import pprint
-import re
-import urllib # Used to unescape URL parameters.
-import logging
 import cgi
+import logging
+import pprint
+import random
+import re
+import urllib
 
 # Core/AppEngine stuff
 import wsgiref.handlers
@@ -78,6 +79,34 @@ def FindEntryToMergeInto(contact, feed):
 
   return None
 
+
+class Updater(object):
+  """Queues up updates and flushes them to gdata batch as needed."""
+
+  def __init__(self, client=None):
+    self.client = client
+    self.batch_feed = gdata.contacts.ContactsFeed()
+
+  def AddInsert(self, entry):
+    self.batch_feed.AddInsert(entry)
+    self.FlushIfNeeded()
+
+  def AddUpdate(self, entry):
+    self.batch_feed.AddUpdate(entry)
+    self.FlushIfNeeded()
+
+  def FlushIfNeeded(self):
+    if len(self.batch_feed.entry) >= 50:   # could be 100 max
+      self.Flush()
+
+  def Flush(self):
+    if not len(self.batch_feed.entry):
+      return
+    self.client.ExecuteBatch(self.batch_feed,
+                             gdata.contacts.service.DEFAULT_BATCH_URL)
+    self.batch_feed = gdata.contacts.ContactsFeed()
+
+
 class AddressBooker(webapp.RequestHandler):
 
   def get(self):
@@ -99,184 +128,6 @@ class AddressBooker(webapp.RequestHandler):
           users.create_login_url('http://%s/merge/' % settings.HOST_NAME)))
     self.response.out.write('</div>')
 
-    # Initialize a client to talk to Google Data API services.
-    client = gdata.service.GDataService()
-    gdata.alt.appengine.run_on_appengine(client)
-
-    # And the subclass of the Service for the Contacts API:
-    contacts_client = contactsservice.ContactsService()
-    gdata.alt.appengine.run_on_appengine(contacts_client)
-
-    session_token = None
-    # Find the AuthSub token and upgrade it to a session token.
-    auth_token = gdata.auth.extract_auth_sub_token_from_url(self.request.uri)
-    if auth_token:
-      # Upgrade the single-use AuthSub token to a multi-use session token.
-      session_token = client.upgrade_to_session_token(auth_token)
-    if session_token and users.get_current_user():
-      # If there is a current user, store the token in the datastore and
-      # associate it with the current user. Since we told the client to
-      # run_on_appengine, the add_token call will automatically store the
-      # session token if there is a current_user.
-      client.token_store.add_token(session_token)
-    elif session_token:
-      # Since there is no current user, we will put the session token
-      # in a property of the client. We will not store the token in the
-      # datastore, since we wouldn't know which user it belongs to.
-      # Since a new client object is created with each get call, we don't
-      # need to worry about the anonymous token being used by other users.
-      client.current_token = session_token
-
-    # Get the URL for the desired feed and get the display option.
-    feed_url = self.request.get('feed_url')
-    erase_tokens = self.request.get('erase_tokens')
-    if erase_tokens:
-      self.EraseStoredTokens()
-    show_xml = self.request.get('xml')
-
-    if show_xml:
-      checked_string = 'checked'
-    else:
-      checked_string = ''
-      
-    self.response.out.write("""<div id="wrap"><div id="header">
-          <h1>AddressBooker</h1>
-          <form action="/" method="get">
-          <label id="feed_url_label" for="feed_url">Target URL:</label>
-          <input type="text" size="60" name="feed_url" id="feed_url" 
-              value="%s"></input>
-          <input type="submit" value="Fetch Atom"></input>
-          <label for="xml">Show XML:</label>
-          <input type="checkbox" id="xml" name="xml" value="true" %s></input>
-        </form></div>""" % ((feed_url or ''), checked_string))
-
-    self.response.out.write('<div id="main">')
-    if not feed_url:
-      self.ShowInstructions()
-    else:
-      self.FetchFeed(client, feed_url, show_xml)
-    self.response.out.write('</div>')
-
-    if users.get_current_user():
-      self.response.out.write("""<div id="sidebar"><div id="scopes">
-          <h4>Request a token for some common scopes</h4><ul>
-          <li><a href="%s">Blogger</a></li>
-          <li><a href="%s">Calendar</a></li>
-          <li><a href="%s">Google Documents</a></li>
-          </ul></div><div id="tokens">""" % (
-              self.GenerateScopeRequestLink(client, 
-                  'http://www.blogger.com/feeds/'),
-              self.GenerateScopeRequestLink(client, 
-                  'http://www.google.com/calendar/feeds'),
-              self.GenerateScopeRequestLink(client, 
-                  'http://docs.google.com/feeds/')))
-
-      self.DisplayAuthorizedUrls()
-      self.response.out.write('</div>')
-    self.response.out.write('</div></div></body></html>')
-    
-  def GenerateScopeRequestLink(self, client, scope):
-    return client.GenerateAuthSubURL('http://%s/' % (
-            settings.HOST_NAME,),
-        scope, secure=False, session=True)
-
-  def ShowInstructions(self):
-    self.response.out.write(
-      """<p>This sample application illustrates the
-        use of <a 
-        href="http://code.google.com/apis/accounts/docs/AuthForWebApps.html">
-        AuthSub authentication</a> to access 
-        <a href="http://code.google.com/apis/gdata/">Google Data feeds</a>.</p>
-      """)
-
-
-  def GenerateFeedRequestLink(self, feed_url):
-    return atom.url.Url('http', settings.HOST_NAME, path='/', 
-        params={'feed_url':feed_url}).to_string()
-
-  def FetchFeed(self, client, feed_url, show_xml=False):
-    # Attempt to fetch the feed.
-    try:
-      if show_xml:
-        response = client.Get(feed_url, converter=str)
-        response = response.decode('UTF-8')
-        self.response.out.write(cgi.escape(response))
-      else:
-        response = client.Get(feed_url)
-        if isinstance(response, atom.Feed):
-          self.RenderFeed(response)
-        elif isinstance(response, atom.Entry):
-          self.RenderEntry(response)
-        else:
-          self.response.out.write(cgi.escape(response.read()))
-    except gdata.service.RequestError, request_error:
-      # If fetching fails, then tell the user that they need to login to
-      # authorize this app by logging in at the following URL.
-      if request_error[0]['status'] == 401:
-        # Get the URL of the current page so that our AuthSub request will
-        # send the user back to here.
-        next = self.request.uri
-        auth_sub_url = client.GenerateAuthSubURL(next, feed_url,
-            secure=False, session=True)
-        self.response.out.write('<a href="%s">' % (auth_sub_url))
-        self.response.out.write(
-            'Click here to authorize this application to view the feed</a>')
-      else:
-        self.response.out.write(
-            'Something else went wrong, here is the error object: %s ' % (
-                str(request_error[0])))
-
-  def RenderFeed(self, feed):
-    self.response.out.write('<h2>Feed Title: %s</h2>' % (
-        feed.title.text.decode('UTF-8')))
-    for link in feed.link:
-      self.RenderLink(link)
-    for entry in feed.entry:
-      self.RenderEntry(entry)
-
-  def RenderEntry(self, entry):
-    if entry.title and entry.title.text:
-      self.response.out.write('<h3>Entry Title: %s</h3>' % (
-        entry.title.text.decode('UTF-8')))
-    else:
-      self.response.out.write("<h3>(title-less entry)</h3>");
-      
-    if entry.content and entry.content.text:
-      self.response.out.write('<p>Content: %s</p>' % (
-          entry.content.text.decode('UTF-8')))
-    elif entry.summary and entry.summary.text:
-      self.response.out.write('<p>Summary: %s</p>' % (
-          entry.summary.text.decode('UTF-8')))
-      
-    for link in entry.link:
-      self.RenderLink(link)
-
-  def RenderLink(self, link):
-    if link.rel == 'alternate' and link.type == 'text/html':
-      self.response.out.write(
-          'Link: <a href="%s">alternate HTML</a><br/>' % link.href)
-    elif link.type == 'application/atom+xml':
-      self.response.out.write(
-          'Link: <a href="/?feed_url=%s">Fetch %s link (%s)</a><br/>' % (
-              urllib.quote_plus(link.href), link.rel, link.type))
-    else:
-      self.response.out.write(
-          'Link: <a href="%s">%s link (%s)</a><br/>' % (link.href, link.rel,
-              link.type))
-    
-  def DisplayAuthorizedUrls(self):
-    self.response.out.write('<h4>Stored Authorization Tokens</h4><ul>')
-    tokens = gdata.alt.appengine.load_auth_tokens()
-    for token_scope in tokens:
-      self.response.out.write('<li><a href="/?feed_url=%s">%s*</a></li>' % (
-          urllib.quote_plus(str(token_scope)), str(token_scope)))
-    self.response.out.write(
-        '</ul>To erase your stored tokens, <a href="%s">click here</a>' % (
-            atom.url.Url('http', settings.HOST_NAME, path='/', 
-                params={'erase_tokens':'true'}).to_string()))
-
-  def EraseStoredTokens(self):
-    gdata.alt.appengine.save_auth_tokens({})
 
   def post(self):
     handle = self.request.get('handle')
@@ -402,6 +253,20 @@ class MergeGoogle(webapp.RequestHandler):
 
     full_feed_url = contacts_url + "?max-results=99999"
     feed = client.Get(full_feed_url, converter=gdata.contacts.ContactsFeedFromString)
+
+    updater = Updater(client=client);
+
+    if True:
+      new_entry = gdata.contacts.ContactEntry()
+      new_entry.title = atom.Title(text="TEST ENTRY " + str(random.randint(1, 100)))
+      new_entry.email.append(gdata.contacts.Email(
+          rel='http://schemas.google.com/g/2005#work', 
+          address='TESTTEST@gmail.com'))
+      new_entry.phone_number.append(gdata.contacts.PhoneNumber(
+          rel='http://schemas.google.com/g/2005#mobile', text='(206)555-1212'))
+      new_entry.content = atom.Content(text='Test Notes')
+      updater.AddInsert(new_entry)
+      updater.Flush()
 
     for contact in contacts:
       out("<br clear='both'><h2>%s</h2>" % contact["name"])
