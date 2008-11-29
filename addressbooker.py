04acea54f7aaa0d05719333c49d1b729e733cf43
Brad Fitzpatrick <brad@danga.com>
2008-11-29 13:40:46 -0800

    gdata contacts stuff working, including login

--- addressbooker.py
+++ addressbooker.py
@@ -15,26 +15,38 @@
 
 __author__ = 'brad@danga.com (Brad Fitzpatrick)'
 
-import models
-
+# Core Python
+import pprint
+import re
+import urllib # Used to unescape URL parameters.
+import logging
 import cgi
+
+# Core/AppEngine stuff
 import wsgiref.handlers
 from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext import db
+from google.appengine.ext.webapp import template
 from google.appengine.api import urlfetch
-import urllib # Used to unescape URL parameters.
-import gdata.service
-import gdata.alt.appengine
-import gdata.contacts.service as contactsservice
-import gdata.auth
+
+# Libraries included w/ app
 import atom
 import atom.http_interface
 import atom.token_store
 import atom.url
-import settings
+import gdata.alt.appengine
+import gdata.auth
+import gdata.contacts.service as contactsservice
+import gdata.service
 import simplejson
-import pprint
+
+# App stuff
+import settings
+import models
+
+VALID_HANDLE = re.compile(r"^\w+$")
+
 
 class AddressBooker(webapp.RequestHandler):
 
@@ -139,71 +151,14 @@ class AddressBooker(webapp.RequestHandler):
         scope, secure=False, session=True)
 
   def ShowInstructions(self):
-    self.response.out.write("""<p>This sample application illustrates the
+    self.response.out.write(
+      """<p>This sample application illustrates the
         use of <a 
         href="http://code.google.com/apis/accounts/docs/AuthForWebApps.html">
         AuthSub authentication</a> to access 
         <a href="http://code.google.com/apis/gdata/">Google Data feeds</a>.</p>
+      """)
 
-        <p>To start using this sample, you can enter the URL for an Atom feed
-        or entry in the input box above, or you can click on one of the 
-        example feeds below. Some of these feeds require you to authorize this
-        app to have access. To avoid authorizing over and over, sign in to
-        this application above and previous authorization tokens will be stored
-        for future use.</p>
-
-        <h4>Sample Feed URLs</h4>
-        <h5>Publicly viewable feeds</h5>
-        <ul><li><a href="%s"
-        >Recent posts in the Google App Engine blog</a></li>
-        <li><a href="%s"
-        >Recent posts in the Google Data APIs blog</a></li>
-        <li><a href="%s"
-        >See events on the Google Developer Events calendar</a></li></ul>
-        <h5>Feeds which require authorization</h5>
-        <ul><li><a href="%s"
-        >List your Google Documents</a></li>
-        <li><a href="%s"
-        >List the Google Calendars that you own</a></li>
-        <li><a href="%s"
-        >List your Google Calendars</a></li>
-        <li><a href="%s"
-        >List your blogs on Blogger</a></li>
-        <!--<li><a href="%s"
-        >List your Gmail Contacts</a></li>--></ul>
-        
-        <p>To learn more about how this sample works, read the article on
-        <a href="http://code.google.com/appengine/articles/gdata.html"
-        >Retrieving Authenticated Google Data Feeds with
-        Google App Engine</a>.</p>
- 
-        <p>In addition to reading information in Google Data feeds, 
-        it is also possible to write to some of the Google Data services
-        once the user has granted permission to your app. For more details
-        on the capabilities of the different Google Data service see the
-        <a href="http://code.google.com/apis/gdata/">home page</a> for 
-        Google Data APIs.</p>
-
-        <p>This app uses the <a 
-        href="http://code.google.com/p/gdata-python-client/"
-        >gdata-python-client</a> library.</p>
-    """ % (
-        self.GenerateFeedRequestLink(
-            'http://www.blogger.com/feeds/8501956666581132164/posts/default'),
-        self.GenerateFeedRequestLink(
-            'http://googledataapis.blogspot.com/feeds/posts/default'),
-        self.GenerateFeedRequestLink(
-            'http://www.google.com/calendar/feeds/developer-calendar@google.com/public/basic'),
-        self.GenerateFeedRequestLink(
-            'http://docs.google.com/feeds/documents/private/full'),
-        self.GenerateFeedRequestLink(
-            'http://www.google.com/calendar/feeds/default/owncalendars/full'),
-        self.GenerateFeedRequestLink(
-            'http://www.google.com/calendar/feeds/default/allcalendars/full'),
-        self.GenerateFeedRequestLink(
-            'http://www.blogger.com/feeds/default/blogs'),
-        self.GenerateFeedRequestLink(
-            'http://www.google.com/m8/feeds/contacts/default/base')))
 
   def GenerateFeedRequestLink(self, feed_url):
     return atom.url.Url('http', settings.HOST_NAME, path='/', 
@@ -295,6 +250,11 @@ class AddressBooker(webapp.RequestHandler):
 
   def post(self):
     handle = self.request.get('handle')
+    if not handle:
+      raise "Missing argument 'handle'"
+    if not VALID_HANDLE.match(handle):
+      raise "Bogus handle."
+
     json = self.request.get('json')
     group = self.request.get('group')
     
@@ -306,14 +266,117 @@ class AddressBooker(webapp.RequestHandler):
       post_dump.put()
       
     contacts = simplejson.loads(json)
+
+    self.response.out.write(template.render('now_what.html', {
+      'n_contacts': len(contacts),
+      'handle': str(post_dump.key()),
+    }))
+    
     #self.response.out.write("You posted: " + pprint.pformat(contacts));
+
+
+class MergeView(webapp.RequestHandler):
+  """View the contacts for a given handle."""
+
+  def get(self):
+    key = self.request.get('key')
+    if not key:
+      raise "Missing argument 'key'"
+    post_dump = models.PostDump.get(db.Key(key))
+    if not post_dump:
+      raise "State lost?  Um, do it again."
+
+    contacts = simplejson.loads(post_dump.json)
     for contact in contacts:
       self.response.out.write("<br clear='both'><h2>%s</h2>" % contact["name"])
       self.response.out.write("<img src='%s' style='float:left' />" % contact["img"])
       for number in contact["numbers"]:
+        obf_number = re.sub(r"\d{3}$", "<i>xxx</i>", number["number"])
         self.response.out.write("<p><b>%s</b> %s</p>" % (
-          number["type"], number["number"]))
-            
+          number["type"], obf_number))
+
+
+class MergeGoogle(webapp.RequestHandler):
+  """Merge contacts into Google Contacts w/ Google Contacts API."""
+
+  def get(self):
+    key = self.request.get('key')
+    if not key:
+      raise "Missing argument 'key'"
+    post_dump = models.PostDump.get(db.Key(key))
+    if not post_dump:
+      raise "State lost?  Um, do it again."
+
+    user = users.get_current_user()
+    logging.info("Current user: " + str(user))
+
+    # We need a logged-in user for the GData.client.token_store to work.
+    if not user:
+      logging.info("Redirecting to sign-in.");
+      sign_in_url = users.create_login_url('http://%s/merge/google?key=%s' %
+                                           (settings.HOST_NAME, key))
+      self.redirect(sign_in_url)
+      return
+
+    # And the subclass of the Service for the Contacts API:
+    client = contactsservice.ContactsService()
+    gdata.alt.appengine.run_on_appengine(client)
+
+    contacts = simplejson.loads(post_dump.json)
+
+    contacts_url = "http://www.google.com/m8/feeds/contacts/default/full"
+    auth_base_url = "http://www.google.com/m8/feeds/"
+
+    session_token = client.token_store.find_token(auth_base_url)
+    if type(session_token) == atom.http_interface.GenericToken:
+      session_token = None
+
+    if not session_token:
+      # Find the AuthSub token and upgrade it to a session token.
+      auth_token = gdata.auth.extract_auth_sub_token_from_url(self.request.uri)
+      if auth_token:
+        session_token = client.upgrade_to_session_token(auth_token)
+        client.token_store.add_token(session_token)
+        # just to sanitize our URL:
+        self.redirect('http://%s/merge/google?key=%s' %
+                      (settings.HOST_NAME, key))
+      else:
+        next = self.request.uri
+        auth_sub_url = client.GenerateAuthSubURL(next, auth_base_url,
+                                                 secure=False, session=True)
+        self.redirect(str(auth_sub_url))
+      return
+
+    self.response.out.write("We're good to go.");
+    sign_out_url = users.create_logout_url('http://%s/merge/' %
+                                          (settings.HOST_NAME))
+    self.response.out.write("\nOr you want to <a href='%s'>log out</a>?" % sign_out_url)
+
+    groups_feed = client.Get("http://www.google.com/m8/feeds/groups/default/full")
+    self.response.out.write(cgi.escape(str(groups_feed)))
+
+    full_feed_url = contacts_url + "?max-results=99999"
+    feed = client.Get(full_feed_url, converter=gdata.contacts.ContactsFeedFromString)
+    for entry in feed.entry:
+      if entry.title and entry.title.text:
+        self.response.out.write('<h3>Entry Title: %s</h3>' % (
+            entry.title.text.decode('UTF-8')))
+      else:
+        self.response.out.write("<h3>(title-less entry)</h3>");
+      
+      for phone_number in entry.phone_number:
+        self.response.out.write("<p><b>Phone: (%s)</b> %s</p>" %
+                                (phone_number.rel, phone_number.text))
+
+      for email in entry.email:
+        self.response.out.write("<p><b>Email: (%s)</b> %s</p>" %
+                                (email.rel, email.address))
+
+      for group in entry.group_membership_info:
+        self.response.out.write("<p><b>Group: (%s)</b> %s</p>" %
+                                (group.href, cgi.escape(str(group))))
+
+    
 
 class Acker(webapp.RequestHandler):
   """Simulates an HTML page to prove ownership of this domain for AuthSub 
@@ -325,9 +388,12 @@ class Acker(webapp.RequestHandler):
 
 
 def main():
-  application = webapp.WSGIApplication([('/merge/', AddressBooker), 
-      ('/google72db3d6838b4c438.html', Acker)], 
-      debug=True)
+  application = webapp.WSGIApplication([
+    ('/merge/', AddressBooker),
+    ('/merge/google', MergeGoogle), 
+    ('/merge/view', MergeView), 
+    ('/google72db3d6838b4c438.html', Acker),
+    ], debug=True)
   wsgiref.handlers.CGIHandler().run(application)
 
 
